6e80c1977674d73502c97033d55b2d392e017b42
SanathKumarMS <sanathkumar.ms@ymedialabs.com>
2019-07-31 14:41:03 +0530

    Var and const

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,23 @@
 import UIKit
 
+
+//Variables and constants
+var variable = 3
+variable = 10
+
+let cons = 1
+
+var str : String = "Hello"
+var count : Int = 5
+var perc : Float = 4.0
+var doub : Double = 11.0
+
+print(type(of : perc), terminator : "")
+
+//String concatenation
 var s1 = "Sanath"
 var s2 = "Kumar"
 var s = s1 + " " + s2
+
+
+
